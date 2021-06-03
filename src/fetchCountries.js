ced7343735f75fd39f13a03f771f8963ef99401b
Tetiana Yarova <andronikova11@gmail.com>
2021-06-03 15:22:42 +0300

    Добавляет функцию поиска

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,23 +3,38 @@ import countryCardTpl from './templates/country-card.hbs';
 
 const refs = {
   cardContainer: document.querySelector('.js-card-container'),
+  searchForm: document.querySelector('.js-search-form'),
 };
 
-fetchCountry();
+refs.searchForm.addEventListener('submit', onSearch);
 
-function fetchCountry() {
-  fetch('https://restcountries.eu/rest/v2/name/ukraine')
-    .then(response => {
-      return response.json();
-    })
+fetchCountry()
+  .then(renderCountryCard)
+  .catch(error => {
+    console.log(error);
+  });
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
+  fetchCountry(searchQuery)
     .then(renderCountryCard)
     .catch(error => {
       console.log(error);
-    });
+    })
+    .finally(() => form.reset());
 }
 
-function renderCountryCard(name) {
-  const markup = countryCardTpl(...name);
+function fetchCountry(countryName) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`).then(response => {
+    return response.json();
+  });
+}
+
+function renderCountryCard(country) {
+  const markup = countryCardTpl(...country);
   refs.cardContainer.innerHTML = markup;
   // console.log(markup);
 }
