de1aa4207f37d50b34eaf36ec046b5196d964e63
Tetiana Yarova <andronikova11@gmail.com>
2021-06-03 21:07:38 +0300

    Создает функции поиска

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,40 +1,29 @@
 import './sass/main.scss';
 import countryCardTpl from './templates/country-card.hbs';
+import API from './api-service.js';
+import getRefs from './get-refs.js';
 
-const refs = {
-  cardContainer: document.querySelector('.js-card-container'),
-  searchForm: document.querySelector('.js-search-form'),
-};
+const refs = getRefs();
 
 refs.searchForm.addEventListener('submit', onSearch);
-
-fetchCountry()
-  .then(renderCountryCard)
-  .catch(error => {
-    console.log(error);
-  });
 
 function onSearch(e) {
   e.preventDefault();
 
   const form = e.currentTarget;
   const searchQuery = form.elements.query.value;
-  fetchCountry(searchQuery)
-    .then(renderCountryCard)
-    .catch(error => {
-      console.log(error);
-    })
-    .finally(() => form.reset());
-}
 
-function fetchCountry(countryName) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`).then(response => {
-    return response.json();
-  });
+  API.fetchCountries(searchQuery)
+    .then(renderCountryCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
 }
 
 function renderCountryCard(country) {
   const markup = countryCardTpl(...country);
   refs.cardContainer.innerHTML = markup;
-  // console.log(markup);
+}
+
+function onFetchError(error) {
+  alert('It is not find. Let`s try again');
 }
